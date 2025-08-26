cc5d48a326f50ee419df202348d1420efd4b4ceb
DrDavid31 <david311097311097@gmail.com>
2025-08-26 10:25:16 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,16 @@
  */
 
 const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
+  if (!elem) return;
+  if (elem.length && elem.length > 1) {
     for (let i = 0; i < elem.length; i++) {
       elem[i].addEventListener(type, callback);
     }
+     } else if (elem.length === 1) {
+    elem[0].addEventListener(type, callback);
   } else {
     elem.addEventListener(type, callback);
   }
-}
-
 
 
 /**
@@ -23,15 +24,17 @@ const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
 const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  navToggler.classList.toggle("active");
-}
+  navbar?.classList.toggle("active");
+  navToggler?.classList.toggle("active");
+};
 
 addEventOnElem(navToggler, "click", toggleNavbar);
 
 const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
+  navbar?.classList.remove("active");
+  navToggler?.classList.remove("active");
+};
+
 }
 
 addEventOnElem(navbarLinks, "click", closeNavbar);
@@ -47,16 +50,17 @@ const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function () {
   if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
+     header?.classList.add("active");
+    backTopBtn?.classList.add("active");
   } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
+  header?.classList.remove("active");
+    backTopBtn?.classList.remove("active");
   }
 });
 
 window.addEventListener("load", function () {
   const preloader = document.getElementById("preloader");
+   if (!preloader) return;
   setTimeout(function () {
     preloader.classList.add("hidden");
   }, 3000);
